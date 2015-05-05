1d8b494a74f1e606509f1b7569a728de9ae6d98f
Stephan Galea <stephan.galea@gmail.com>
2015-05-05 20:24:21 +0200

    first commit

---
+++ app/app.module.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="c:\users\stephan\documents\visual studio 2013\Projects\DietMalta\DietMalta.Web\Scripts/angular.js" />
+/// <reference path="c:\users\stephan\documents\visual studio 2013\Projects\DietMalta\DietMalta.Web\Scripts/angular-route.js" />
+
+(function () {
+    'use strict';
+    angular.module("app", [
+        // Angular Modules
+        'ngRoute',
+        // App Modules
+        
+        'app.posts'
+        
+        // 3rd Party Modules
+    ]);
+})();
+
+
+
+(function () {
+    'use strict';
+    angular.module("app").config(['$routeProvider',
+        function ($routeProvider) {
+        $routeProvider
+            .when('/Posts', {
+                templateUrl: 'posts/posts.html',
+                controller: 'posts/posts.js'
+            })
+            .otherwise({
+                rediretTo:"/"
+    });
+    }]);
+})();
+
---
+++ app/posts/posts.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    // Register
+    angular
+        .module('app.posts')
+        .controller('Posts',Posts);
+
+    // Inject
+    Posts.$inject =  ['$http'];
+
+    // Function
+    function Posts($http) {
+        
+        var vm = this;
+
+        $http.get('../mockData/posts.json')
+            .success(function (data, status, header, config) {
+                vm.posts = data;
+            })
+            .error(function (data, status, header, config) {
+                console.log('Error');
+
+            });
+    }
+
+})();
+
+
